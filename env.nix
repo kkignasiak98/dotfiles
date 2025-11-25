a6915cfa4df9fd2eab87b06c354895fa142b103b
kkignasiak98 <141998545+kkignasiak98@users.noreply.github.com>
2025-11-25 13:59:55 +0000

    initial verion of env.nix

---
+++ env.nix
@@ -0,0 +1,7 @@
+with import (fetchTarball "https://github.com/NixOS/nixpkgs/archive/nixpkgs-unstable.tar.gz") {}; [
+  obsidian
+  zed-editor
+  pcloud
+  megasync
+  #zen-browser # currently broken
+]
